f0ad60beaa6087f94b10dd02e239d7ce50846fcd
Vasile Pop <vasile.pop@gmail.com>
2015-05-21 10:31:27 +0000

    fixed adding clients

--- server/methods/clientMethods.js
+++ server/methods/clientMethods.js
@@ -1,6 +1,6 @@
 Meteor.methods({
     addClient: function(doc) {
-        check(doc, Schemas.Client);
+        check(doc, ClientSchema);
         if (Roles.userIsInRole(Meteor.user(), [SUPERADMIN])) {
             Clients.insert(doc);
         } else throw new Meteor.Error(403, "Not authorized to create new clients");
